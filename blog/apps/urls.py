b8ae390c62babc82ad159eab69ae49beff0bc194
devdeptraino1 <tao.gboy@gmail.com>
2021-08-03 09:31:06 +0700

    adafd

--- blog/apps/urls.py
+++ blog/apps/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.apps.views import index
+from blog.apps.views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('page2', page2),
 ]
--- blog/apps/views.py
+++ blog/apps/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required
+def page2(request):
+    return render(request, 'page2.html')
 # Create your views here.
