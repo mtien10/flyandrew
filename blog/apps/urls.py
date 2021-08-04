f116cdfe36c472de4c581b0a608ec301d5e69f1f
devdeptraino1 <tao.gboy@gmail.com>
2021-08-04 16:57:26 +0700

    sddds

--- blog/apps/urls.py
+++ blog/apps/urls.py
@@ -3,5 +3,18 @@ from blog.apps.views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('page2', page2),
+    #Category
+    path('staff', listCategory),
+    path('staff/create-category', CategoryCreateView.as_view()),
+    path('staff/update-category/<pk>', CategoryUpdateView.as_view()),
+    path('staff/delete-category/<pk>', deleteCategory),
+    #Product
+    path('staff/list-product', listProduct),
+    path('staff/create-product', ProductCreateView.as_view()),
+    path('staff/update-product/<pk>', ProductUpdateView.as_view()),
+    path('staff/delete-product/<pk>', deleteProduct),
+    #Order
+    path('staff/list-order', listOrder),
+    path('staff/view-order/<pk>', viewOrder),
+
 ]
--- blog/apps/views.py
+++ blog/apps/views.py
@@ -1,5 +1,10 @@
+import json
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import *
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
 
 @login_required
@@ -7,7 +12,86 @@ def index(request):
     return render(request, 'index.html')
 
 
-@login_required
-def page2(request):
-    return render(request, 'page2.html')
-# Create your views here.
+# Category
+def listCategory(request):
+    categoryList = Category.objects.all()
+    context = {'categoryList': categoryList}
+    return render(request, 'staff/category/list.html', context)
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'staff/category/form.html'
+    success_url = '/staff'
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'staff/category/form.html'
+    success_url = '/staff'
+
+
+def deleteCategory(request, pk):
+    error = ''
+    success = False
+    if request.method == 'POST':
+        try:
+            c = Category.objects.get(pk=pk)
+            c.delete()
+            success = True
+        except Exception as e:
+            error = str(e)
+    else:
+        error = 'Method not allow'
+
+    result = {'success': success, 'errors': error}
+    return HttpResponse(json.dumps(result), content_type='application/json')
+
+
+# Product
+def listProduct(request):
+    productList = Product.objects.all()
+    context = {'productList': productList}
+    return render(request, 'staff/product/list.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+    template_name = 'staff/product/form.html'
+    success_url =  '/staff/list-product'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name = 'staff/product/form.html'
+    success_url =  '/staff/list-product'
+
+
+def deleteProduct(request, pk):
+    error = ''
+    success = False
+    if request.method == 'POST':
+        try:
+            p = Product.objects.get(pk=pk)
+            p.delete()
+            success = True
+        except Exception as e:
+            error = str(e)
+    else:
+        error = 'Method not allow'
+
+    result = {'success': success, 'errors': error}
+    return HttpResponse(json.dumps(result), content_type='application/json')
+
+
+
+# Order
+def listOrder(request):
+    return render(request, 'staff/order/list.html')
+
+
+def viewOrder(request, pk):
+    return render(request, 'staff/order/view_detail.html')
