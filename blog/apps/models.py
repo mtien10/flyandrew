f53e2941fc8558ea0ee9ba2b50f328521f598015
devdeptraino1 <tao.gboy@gmail.com>
2021-10-12 17:20:14 +0700

    a

--- blog/apps/models.py
+++ blog/apps/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
-
+# abc
 class Category(models.Model):
     code = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200)
