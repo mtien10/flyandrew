fe2540a0c158d5f86709a4e95a1b2b277a8a2cc9
devdeptraino1 <tao.gboy@gmail.com>
2021-10-12 17:02:07 +0700

    add

--- blog/apps/models.py
+++ blog/apps/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Category(models.Model):
     code = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=200)
